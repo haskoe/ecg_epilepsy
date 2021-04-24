6a39faf7caf92f54059b6c31029fa660d5a14d4f
haskoe <henrik@haskoe.dk>
2021-04-24 09:43:40 +0200

    easier annotation plot, load from syncthing dir

--- load_params.m
+++ load_params.m
@@ -11,7 +11,11 @@ function params = load_params()
     
     params.sf = 512;
     params.patient_info_path='patient_info';
+
     params.mat_file_path='mat';
+    if ~isdir(params.mat_file_path)
+        params.mat_file_path='~/sync/philip-public/dtu/ecg_epilepsy/mat';
+    end
     params.mat_filename_start='patient-all-';
     params.tdms_file_path = '**/*.tdms';
 end
--- plot_annotation.m
+++ plot_annotation.m
@@ -3,14 +3,16 @@ params = load_params();
 pid = '16';
 patient = load_patient(pid,params);
 episode = patient.episodes(1,:);
+
 seizure_samples = patient.samples(episode(1,1)+1:episode(1,1)+episode(1,2));
-plot(seizure_samples);
+x=1:length(seizure_samples);
+annotations=400:400:20005;
+ex=annotations;
+ey=arrayfun(@(x) seizure_samples(x),ex);
 
-annotations=1:400:20000;
-
+eb_height=0.1*(max(seizure_samples(1:1000))-min(seizure_samples(1:1000)));
+err=eb_height*ones(length(ex),1);
 hold on
-plot(1:length(seizure_samples),seizure_samples)
-bar(annotations,ones(length(annotations),1)*max(seizure_samples),'BarWidth', 0.02);
+plot(x,seizure_samples);
+errorbar(ex,ey,err);
 hold off
-
-
