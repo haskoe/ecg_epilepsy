f67b43d5f7f62141d3e8ca19eaf1142bd7ab8953
Philip <philiphaskoe@gmail.com>
2021-04-29 19:33:49 +0200

    awewergsetg

--- make_butter.m
+++ make_butter.m
@@ -1,5 +1,5 @@
 function f = make_butter(order,cutoff_frac)
-    [B_high,A_high] = butter(order,cutoff_frac,'high');
+    [B_high,A_high] = butter(order,cutoff_frac,'bandpass');
     
     function filtered = filter_func(samples)
         filtered = filtfilt(B_high,A_high,samples)';
