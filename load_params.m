5b143748c77370d8e248713d34a5ae6adfe5c424
haskoe <henrik@haskoe.dk>
2021-05-12 08:30:56 +0200

    comments

--- load_params.m
+++ load_params.m
@@ -1,25 +1,23 @@
+% common parameters used by most the scripts
 function params = load_params()
-    
-% parameters below are no longer used due to use of new QRS detection
-    % algorithm
-    load s_b_coeff.mat;
-    
     % Add TDMS scripts to MATLAB path
-    addpath( genpath('v2p6') );
+    addpath(genpath('v2p6'));
 
-    params.b_avg = b_avg;
-    params.b_high = b_high;
-    params.b_low = b_low;
-    params.delay = delay;
-    %params.desc_filt = desc_filt;
-    
+    % sample frequency
     params.sf = 512;
-    params.patient_info_path='patient_info';
 
-    params.mat_file_path='mat';
+    params.patient_info_path = 'patient_info';
+
+    % patient mat file path
+    params.mat_file_path = 'mat';
+
+    % fallback patient mat file path
     if ~isdir(params.mat_file_path)
-        params.mat_file_path='~/sync/philip-public/dtu/ecg_epilepsy/mat';
+        params.mat_file_path = '~/sync/philip-public/dtu/ecg_epilepsy/mat';
     end
-    params.mat_filename_start='patient-all-';
+
+    % used by load_patient
+    params.mat_filename_start = 'patient-all-';
+    
     params.tdms_file_path = '**/*.tdms';
 end
